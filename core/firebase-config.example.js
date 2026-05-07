add2e58a8bfe13501db6e81325d770875369bc3a
mtreck <mateus8reck@hotmail.com>
2026-05-07 14:27:22 -0300

    Atualização de segurança

--- core/firebase-config.example.js
+++ core/firebase-config.example.js
@@ -1,10 +1,9 @@
-// Exemplo de configuração. Renomeie para firebase-config.js e preencha com seus dados.
 export const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "SUA_NOVA_CHAVE_AQUI",
   authDomain: "seu-projeto.firebaseapp.com",
   projectId: "seu-projeto",
   storageBucket: "seu-projeto.firebasestorage.app",
   messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:abcdefg",
-  measurementId: "G-ABCDEFG"
+  appId: "1:000000000000:web:000000000000",
+  measurementId: "G-XXXXXXXXXX"
 };
